3191639e9ff437188cab14d4664073b0981bcdac
Nicholas Wickmann <nicowick@icloud.com>
2022-10-28 16:17:29 +0200

    NAW Movie List

--- db/migrate/20221027110010_create_movies.rb
+++ db/migrate/20221027110010_create_movies.rb
@@ -5,7 +5,6 @@ class CreateMovies < ActiveRecord::Migration[7.0]
       t.string :overview
       t.string :poster_url
       t.integer :rating
-      t.references :list, null: false, foreign_key: true
 
       t.timestamps
     end
